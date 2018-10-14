9d6f40e3b150195e419c6ef632e721eede68cdbf
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2018-10-14 14:13:43 -0400

    Working on functions

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 $('#box').click(function() {
 
+})
+
+var score= 0;
+function one-one() {
+    var rightAnswer = document.getElementById('#correct')
+    var wrongAnswer = document.getElementById('#wrong')
 }
