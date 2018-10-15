46eaca425b768e4318c502733b9fd4dbc9f9a683
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2018-10-15 10:06:36 -0400

    Making progress

--- app.js
+++ app.js
@@ -13,3 +13,375 @@ Wm100.addEventListener("click", function () {
     scoreBoard.innerHTML = points;
     console.log(123)
 })
+
+Cv100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wv100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+Cp100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wp100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
+Cm100.addEventListener("click", function () {
+    points += 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+    
+})
+Wm100.addEventListener("click", function () {
+    points -= 100;
+    scoreBoard.innerHTML = points;
+    console.log(123)
+})
+
